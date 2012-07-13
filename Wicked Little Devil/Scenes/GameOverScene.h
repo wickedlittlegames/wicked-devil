97ac7c1731b34c71f850355ec7e63158ab3bd3e2
Andy Girvan <girvan.andy@gmail.com>
2012-07-13 16:37:32 +0100

    Gameover Screen Shown

--- Wicked Little Devil/Scenes/GameOverScene.h
+++ Wicked Little Devil/Scenes/GameOverScene.h
@@ -7,9 +7,15 @@
 //
 
 #import "cocos2d.h"
+#import "User.h"
+#import "LevelSelectScene.h"
+#import "LevelScene.h"
 
 @interface GameOverScene : CCLayer {
-    
+    User *user;
 }
-+(CCScene *) scene;
+@property (nonatomic, assign) int score, timebonus, bigs, world, level;
+
++(CCScene *) sceneWithScore:(int)_score timebonus:(int)_timebonus bigs:(int)_bigs forWorld:(int)_world andLevel:(int)_level;
+
 @end
--- Wicked Little Devil/Scenes/GameOverScene.m
+++ Wicked Little Devil/Scenes/GameOverScene.m
@@ -10,14 +10,20 @@
 
 
 @implementation GameOverScene
+@synthesize score, timebonus, bigs, world, level;
 
-+(CCScene *) scene
++(CCScene *) sceneWithScore:(int)_score timebonus:(int)_timebonus bigs:(int)_bigs forWorld:(int)_world andLevel:(int)_level
 {
     // Create a Scene
 	CCScene *scene = [CCScene node];
     
     // Grab the layers
 	GameOverScene *current = [GameOverScene node];
+    [current setScore:_score];
+    [current setTimebonus:_timebonus];
+    [current setBigs:_bigs];
+    [current setWorld:_world];
+    [current setLevel:_level];
     
     // Fill the scene
 	[scene addChild:current];
@@ -29,7 +35,35 @@
 -(id) init
 {
     if( (self=[super init]) ) {
-        // CGSize screenSize = [CCDirector sharedDirector].winSize;
+        //CGSize screenSize = [CCDirector sharedDirector].winSize;
+        //NSString *font = @"Marker Felt";
+        //int fontsize = 18;
+        
+        user = [[User alloc] init];
+        
+        // Place static elements such as background
+        //CCSprite *background = [CCSprite spriteWithFile:@"bg_gameover.png"];
+        //[background setPosition:ccp(screenSize.width/2, screenSize.height/2)];
+        //[self addChild:background];
+        
+        if ( bigs >= 1 )
+        {
+            int facebook_bonus = ( user.isConnectedToFacebook ? 200 : 0 );
+            int score_calc = (score * bigs) + timebonus + facebook_bonus;
+            
+            CCLOG(@"SCORE IS: %i", score_calc);
+            
+            // Step through the animations for the scores
+            // - Show BASIC score
+            // - Add on multiplier for bigs
+            // - Add on time bonus
+            // - Add on potential Facebook bonus!
+            // - Final score on fire if highscore
+        }
+        else 
+        {
+            // don't do much, just show a fail screen
+        }
     }
     return self;
 }
